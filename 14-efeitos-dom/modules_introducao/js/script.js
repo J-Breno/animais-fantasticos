00dc4e6ff543685f865466b460f7cf2ab4bcf7bb
J-Breno <joao.breno85@hotmail.com>
2024-02-14 23:45:53 -0300

    Separando os arquivos em modulos

---
+++ 14-efeitos-dom/modules_introducao/js/script.js
@@ -0,0 +1,12 @@
+// você pode exportar mais de uma função no mesmo arquivo, porém você tem que passar entre chaves, e lá o nome na função importa, tem que ser identica, porem você pode modifica-lo utilizando o as, você também pode importar sem especifiar utilizando o * as, eu posso exportar tudo, não necessariamente tem que ser só função, quando eu fiz um curso antigo de javascript eu tinha que usar use strict, porém agora que usa-se modules, ele automáticamente já fica como use strict
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from "./modules/tab-nav.js";
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+
