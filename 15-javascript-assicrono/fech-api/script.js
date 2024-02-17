792a299e574033c6414c55364852865477dccc8c
J-Breno <joao.breno85@hotmail.com>
2024-02-17 00:58:29 -0300

    Terminando a parte sobe fech, é praticamente o XMLHttpRequest, só muda um pouco a sintaxe

--- 15-javascript-assicrono/fech-api/script.js
+++ 15-javascript-assicrono/fech-api/script.js
@@ -10,7 +10,19 @@ sobre
     console.log(body);
     div.innerHTML = body;
     const titulo = div.querySelector("h1");
-    document.querySelector('h1').innerText = titulo.innerText;
-    
-});
+    document.querySelector("h1").innerText = titulo.innerText;
+  });
 // O objeto Response, possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado utilizando métodos do protótipo do objeto Respose. Estes retornam outras promises.
+// blob é um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transformarmos requisições de imagens por exemplo. O blog gera um URL único.
+
+const imagem = fetch("./spring-3.svg");
+
+imagem
+  .then((r) => r.blob())
+  .then((body) => {
+    const blobUrl = URL.createObjectURL(body);
+    console.log(blobUrl);
+    const imagemNoDom = document.querySelector('img')
+    imagemNoDom.src = blobUrl;
+    console.log(body);
+  });
